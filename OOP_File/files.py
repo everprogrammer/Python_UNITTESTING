16c67c8d9d82f563e802daf4a46b930caaae055f
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-23 12:13:53 +0330

    Create Files class to perform testing on

---
+++ OOP_File/files.py
@@ -0,0 +1,39 @@
+import os
+
+# Set current working direcory
+
+class Files:
+
+    def __init__(self, filename=None):
+        self.cwd = os.path.dirname(os.path.realpath(__file__))
+        self.filename = None
+        if filename:
+            self.set_filename(filename)
+
+    def set_filename(self, filename):
+        self.filename = os.path.join(self.cwd, filename)
+
+    def read_file(self):
+        if self.filename:
+            try:
+                with open(self.filename, 'r') as file_to_read:
+                    data = file_to_read.readlines()
+                return data
+            except FileNotFoundError as e:
+                raise FileNotFoundError(f'{self.filename} does not exist!')
+        else:
+            raise ValueError('Filename is not set!')
+    
+    def write_file(self, content, mode):
+        if self.filename:       
+            with open(self.filename, mode) as file_to_write:
+                file_to_write.writelines(str(content))
+            return 'Writing done!'
+        else:
+            raise ValueError('Filename is not set!')
+
+    def set_memory(self):
+        self.memory = self.read_file()
+
+    def get_memory(self):
+        return self.memory
