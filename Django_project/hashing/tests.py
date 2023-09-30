9d3cde352eb27c61d172a545b1e06f4eeea7adde
everprogrammer <amir.tavakolian68@gmail.com>
2023-09-30 15:45:11 +0330

    - Add test for bad hashing

--- Django_project/hashing/tests.py
+++ Django_project/hashing/tests.py
@@ -3,26 +3,27 @@ from selenium import webdriver
 from .forms import HashForm
 import hashlib
 from .models import Hash
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
 
 # class FunctionalTest(TestCase):
-    # def setUp(self):
-    #     self.browser = webdriver.Chrome()
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
 
-    # def test_homePageExists(self):
-    #     self.browser.get('http://localhost:8000')
-    #     self.assertIn('Enter hash here:', self.browser.page_source)
+#     def test_homePageExists(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('Enter hash here:', self.browser.page_source)
 
-    # def test_helloHashPhrase(self):
-    #     self.browser.get('http://localhost:8000')
-    #     text = self.browser.find_element_by_id('id_text')
-    #     text.send_keys('hello')
-    #     self.browser.find_element_by_name('submit').click()
-    #     self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+#     def test_helloHashPhrase(self):
+#         self.browser.get('http://localhost:8000')
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         self.browser.find_element_by_name('submit').click()
+#         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
-    # def tearDown(self):
-    #     self.browser.quit()    
+#     def tearDown(self):
+#         self.browser.quit()    
 
 
 class UnitTest(TestCase):
@@ -55,6 +56,13 @@ class UnitTest(TestCase):
         hash = self.save_hash()
         response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
         self.assertContains(response, 'hello')
+
+    def test_badData(self):
+        def bad_hash():
+            hash = Hash()
+            hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824AAAA'
+            hash.full_clean()
+        self.assertRaises(ValidationError, bad_hash)
 
 
         
